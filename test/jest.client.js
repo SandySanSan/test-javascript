33e584b9f8e66f41e6745079a9da005052c4feb2
Sandy Prolhac <s.prolhac@asystom.com>
2021-04-15 11:34:22 +0200

    Add server and client jest config

--- test/jest.client.js
+++ test/jest.client.js
@@ -1,25 +1,13 @@
-const path = require('path')
-
 module.exports = {
+  ...require('./jest.common'),
   testEnvironment: 'jest-environment-jsdom',
-  moduleDirectories: [
-    'node_modules',
-    path.join(__dirname, 'src'),
-    'shared',
-    path.join(__dirname, 'test'),
-  ],
-  moduleNameMapper: {
-    '\\.module\\.css$': 'identity-obj-proxy',
-    '\\.css$': require.resolve('./test/style-mock.js'),
-  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
-  collectCoverageFrom: ['**/src/**/*.js'],
   coverageThreshold: {
     global: {
       statements: 31,
       branches: 18,
-      functions: 29,
+      functions: 26,
       lines: 29,
     },
     './src/shared/utils.js': {
